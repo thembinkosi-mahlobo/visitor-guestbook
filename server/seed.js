17c0c7211e86a7c1eda6b5d4bdea3c3889139117
Thembinkosi Mahlobo <themba.romano@hotmail.co.uk>
2024-04-12 15:54:43 +0100

    add changes and update client and server

--- server/seed.js
+++ server/seed.js
@@ -1,6 +1,15 @@
 import Database from "better-sqlite3";
 const db = new Database("database.db");
 
+db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        message INTEGER NOT NULL
+        
+    )
+`);
+
 db.prepare(`INSERT INTO messages (name, message) VALUES (?, ?)`).run(
   "PhatJoe",
   "Argubly the best lyrist"
@@ -13,3 +22,5 @@ const insertStatement = db.prepare(
 insertStatement.run("PhatJoe", "Argubly the best lyrist");
 insertStatement.run("BigPun", "That's an opinion");
 insertStatement.run("hip-hop", "disagrees");
+
+insertStatement.run("PhatJoe", "That's an opinion");
